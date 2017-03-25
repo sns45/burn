7409de1ef26f34bc064a4de4abe7e6b6b8405f8a
Shantanu Sharma <sns45@njit.edu>
2017-03-25 13:18:25 -0400

    nutritionist and workout debugging

--- public/nutritionists/controllers/nutritionists.controller.js
+++ public/nutritionists/controllers/nutritionists.controller.js
@@ -9,29 +9,31 @@
         var url = "";
         var data;
         var nutritionistObj = {
-            "name": "",
-            "address": "",
-            "phone_number": "",
-            "rating": ""
+            "name": ""
+            , "address": ""
+            , "phone_number": ""
+            , "rating": ""
         };
-        var tempArray = [];
+        var tempArray =[];
         var zipcode;
         var userData;
         (function () {
             for (var x = 0; x < 2; x++) {
                 if (x < 1) {
                     userData = CommonSvc.getUserData();
-
-                    if (userData == undefined) {
-                        console.log("Please Provide the your Zip Code");
+                    
+                    if (userData ==undefined) {
+                       console.log("Please Provide the your Zip Code");
                         $location.path('/');
                         break;
-                    } else {
-
-                        zipcode = userData.userObject.profile.zipcode;
+                    }
+                    else {
+                       
+                        zipcode = userData.profile.zipcode;  
                         url = "/api/main/yelp/nutritionists/location/" + zipcode;
                     }
-                } else {
+                }
+                else {
                     $http.get(url).then(function (response) {
                         if (response.data.businesses.length > 5) {
                             response.data.businesses.splice(5, response.data.businesses.length);
@@ -39,7 +41,8 @@
                             vm.businessesArray.forEach(function (value) {
                                 ratingFunc(value);
                             });
-                        } else {
+                        }
+                        else {
                             vm.businessesArray = response.data.businesses;
                         }
                         return response;
@@ -49,35 +52,25 @@
         })();
 
         function selectFunction(obj) {
-            if (obj.selected === undefined || obj.selected === false) {
-                obj.selected = true;
-                nutritionistObj = {
-                    "phone_number": obj.display_phone,
-                    "address": obj.location.display_address[0] + obj.location.display_address[1],
-                    "name": obj.name,
-                    "rating": obj.rating
-                }
-                userData.userObject.nutritionists.push(nutritionistObj);
-                console.log(userData.userObject.nutritionists);
-            } else {
-                userData.userObject.nutritionists.splice(userData.userObject.nutritionists.indexOf(obj), 1);
-                obj.selected = false;
-                console.log(userData.userObject.nutritionists);
-            }
+             nutritionistObj.phone_number = obj.display_phone;
+             nutritionistObj.address = obj.location.display_address[0] + obj.location.display_address[1];
+             nutritionistObj.name = obj.name;
+             nutritionistObj.rating = obj.rating;
+     
         }
 
         function route(param) {
             switch (param) {
-                case 'Back':
-                    console.log(userData);
-                    $location.path('/planning');
-                    break;
-                case 'Save':
-
-                    console.log(userData);
-                    CommonSvc.setUserData(userData);
-                    $location.path('/planning');
-                    break;
+            case 'Back':
+                console.log(userData);
+                $location.path('/planning');
+                break;
+            case 'Save':
+                userData.nutritionists.push(nutritionistObj);
+                console.log(userData);
+                CommonSvc.setUserData(userData);
+                $location.path('/planning');
+                break;
             }
         }
 
@@ -109,7 +102,7 @@
             if (value.rating == 1) {
                 value.rating = "../../documents/stars/1Stars.png";
             }
-            if (value.rating == "" || value.rating == 0) {
+            if(value.rating == ""||value.rating == 0) {
                 value.rating = "../../documents/stars/0Stars.png";
             }
         }
