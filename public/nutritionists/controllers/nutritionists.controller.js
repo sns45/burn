4d73c624fa45dab96159070b836d518219d0ea04
Shantanu Sharma <sns45@njit.edu>
2017-03-25 13:10:28 -0400

    nutritionist check

--- public/nutritionists/controllers/nutritionists.controller.js
+++ public/nutritionists/controllers/nutritionists.controller.js
@@ -1,110 +1,56 @@
-"use strict";
-(function () {
-    angular.module("burnIt.nutritionists").controller("NutritionistsCtrl", ["CommonSvc", "$location", "$http", NutritionistsCtrl]);
+/*"use strict";
 
-    function NutritionistsCtrl(CommonSvc, $location, $http) {
+(function () {
+    angular
+        .module("burnIt.nutritionists")
+        .controller("NutritionistsCtrl", ["CommonSvc", "$location", NutritionistsCtrl]);
+
+    function NutritionistsCtrl(CommonSvc, $location) {
+        var vm = this;
+        vm.backToPlan ={};
+        vm.backtoplanning() = backtoplanning;
+
+        function backtoplanning(){
+             $location.path("/planning");
+        }
+
+    }
+})();*/
+"use strict";
+
+(function () {
+    angular
+        .module("burnIt.nutritionists")
+        .controller("NutritionistsCtrl", ["CommonSvc", "$location", NutritionistsCtrl]);
+     
+    function NutritionistsCtrl(CommonSvc, $location) {
         var vm = this;
         vm.route = route;
-        vm.selectFunction = selectFunction;
-        var url = "";
-        var data;
-        var nutritionistObj = {
-            "name": ""
-            , "address": ""
-            , "phone_number": ""
-            , "rating": ""
-        };
-        var tempArray =[];
-        var zipcode;
-        var userData;
-        (function () {
-            for (var x = 0; x < 2; x++) {
-                if (x < 1) {
-                    userData = CommonSvc.getUserData();
-                    
-                    if (userData ==undefined) {
-                       console.log("Please Provide the your Zip Code");
-                        $location.path('/');
-                        break;
-                    }
-                    else {
-                       
-                        zipcode = userData.profile.zipcode;  
-                        url = "/api/main/yelp/nutritionists/location/" + zipcode;
-                    }
-                }
-                else {
-                    $http.get(url).then(function (response) {
-                        if (response.data.businesses.length > 5) {
-                            response.data.businesses.splice(5, response.data.businesses.length);
-                            vm.businessesArray = response.data.businesses;
-                            vm.businessesArray.forEach(function (value) {
-                                ratingFunc(value);
-                            });
-                        }
-                        else {
-                            vm.businessesArray = response.data.businesses;
-                        }
-                        return response;
-                    });
-                }
-            }
+        
+        (function () {  
+           vm.mame = "Janet Williams";
+           vm.address ="12 Bergen Street NY 10082";
+           vm.phonenumber= "347-890-3787";
         })();
-
-        function selectFunction(obj) {
-             nutritionistObj.phone_number = obj.display_phone;
-             nutritionistObj.address = obj.location.display_address[0] + obj.location.display_address[1];
-             nutritionistObj.name = obj.name;
-             nutritionistObj.rating = obj.rating;
-     
-        }
-
+        
         function route(param) {
-            switch (param) {
-            case 'Back':
-                console.log(userData);
-                $location.path('/planning');
-                break;
-            case 'Save':
-                userData.nutritionists.push(nutritionistObj);
-                console.log(userData);
-                CommonSvc.setUserData(userData);
-                $location.path('/planning');
-                break;
+            switch (param){
+                case 'Back':
+                    $location.path('/planning');
+                    break;
+                case 'Save':
+                    // save logic
+                    // CommonSvc.set
+                    $location.path('/planning');
+                    break;
             }
         }
 
-        function ratingFunc(value) {
-            if (value.rating == 5) {
-                value.rating = "../../documents/stars/5Stars.png";
-            }
-            if (value.rating == 4.5) {
-                value.rating = "../../documents/stars/4.5Stars.png";
-            }
-            if (value.rating == 4) {
-                value.rating = "../../documents/stars/4Stars.png";
-            }
-            if (value.rating == 3.5) {
-                value.rating = "../../documents/stars/3.5Stars.png";
-            }
-            if (value.rating == 3) {
-                value.rating = "../../documents/stars/3Stars.png";
-            }
-            if (value.rating == 2.5) {
-                value.rating = "../../documents/stars/2.5Stars.png";
-            }
-            if (value.rating == 2) {
-                value.rating = "../../documents/stars/2Stars.png";
-            }
-            if (value.rating == 1.5) {
-                value.rating = "../../documents/stars/1.5Stars.png";
-            }
-            if (value.rating == 1) {
-                value.rating = "../../documents/stars/1Stars.png";
-            }
-            if(value.rating == ""||value.rating == 0) {
-                value.rating = "../../documents/stars/0Stars.png";
-            }
-        }
+//        (function() {
+//            if (CommonSvc.getPerson() === undefined) {
+//                $location.path('Start')
+//            }
+//        })
+//
     }
 })();
